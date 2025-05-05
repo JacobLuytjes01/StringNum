b4e128131dd5fa8e1c9b73a7b4006a434baf6d88
Jacob Luytjes <jluytjes01@aurora.edu>
2025-05-05 11:47:16 -0500

    Create StringNum.h

---
+++ StringNum.h
@@ -0,0 +1,39 @@
+//
+// Created by Jacob Luytjes on 3/17/2025.
+//
+
+#ifndef STRINGNUM_H
+#define STRINGNUM_H
+#include <string>
+
+class StringNum {
+private:
+    unsigned int decimal;
+    bool negative = false;
+    std::string number;
+
+    StringNum add(const StringNum& num)const&;
+    StringNum sub(const StringNum& num)const&;
+    void trimBack();
+    void trimFront();
+
+public:
+    StringNum();
+    StringNum(const int& num);
+    StringNum(const StringNum& num);
+    StringNum(const std::string& num);
+
+    StringNum& operator=(const StringNum& num);
+    StringNum operator+ (const StringNum& num) const&;
+    StringNum operator- (const StringNum& num) const&;
+
+    bool operator==(const StringNum& num)const&;
+    bool operator!=(const StringNum& num)const&;
+    bool operator>=(const StringNum& num)const&;
+    bool operator<=(const StringNum& num)const&;
+    bool operator>(const StringNum& num)const&;
+    bool operator<(const StringNum& num)const&;
+
+    std::string getNumber() const ;
+};
+#endif //STRINGNUM_H
