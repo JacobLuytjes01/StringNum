af0802eb91b31fa12f12620cd54720d1530cb882
Jacob Luytjes <jluytjes01@aurora.edu>
2026-01-15 20:41:36 -0600

    Made tests more automatic

--- Tests/main.cpp
+++ Tests/main.cpp
@@ -6,6 +6,28 @@ ostream& operator<<(ostream& out, const StringNum& b){
     out << b.getNumber();
     return out;
 }
+void CheckNum(const StringNum& a, const StringNum& b, bool& check) {
+    if (a != b) {
+        check = false;
+        cout << a.getNumber() << " != " << b.getNumber() << endl;
+    }
+}
+void CheckNum2(const StringNum& a, const string& s, bool& check) {
+    if (!(a >= StringNum(s))) {
+        check = false;
+        cout << ">= failed on "<< a << " : " << s << endl;
+    }
+    if (!(a <= StringNum(s))) {
+        check = false;
+        cout << "<= failed on "<< a << " : " << s << endl;
+    }
+}
+void CheckNumLeftBigger(const StringNum& a, const string& s, bool& check) {
+    if (!(a >= StringNum(s) && a > StringNum(s))) {
+        check = false;
+        cout << "Left not bigger on "<< a << " : " << s << endl;
+    }
+}
 
 int main() {
     StringNum str1("33");
@@ -20,105 +42,114 @@ int main() {
     StringNum str6("555.77");
     StringNum str7("-555.77");
 
-    /*
-    cout << ++str1 << endl; // 34.0
-    cout << str1++ << endl; // 34.0
-    cout << str1 << endl; // 35.0
-    */
+    bool allTestsPass = true;
 
-    /*
-    cout << str1D * str1D << endl; // 2494.0036
-    cout << str2 + str2 << endl; // 58.246246
-    cout << str3 * str3 << endl; // 999998.000001
-    cout << str2 * str1C << endl; // 58.246246
-    cout << str1C * str2 << endl; // 58.246246
-    cout << str7 * str7 << endl; // 308880.2929
-    cout << str1C * str1C << endl; // 4.0
-    cout << str4 * str1C << endl; // 19999.9998
+    CheckNum(++str1, StringNum("34.0"), allTestsPass);
+    CheckNum(str1++, StringNum("34.0"), allTestsPass);
+    CheckNum(str1, StringNum("35.0"), allTestsPass);
 
-    // should both be -0.54
-    cout << str5 + str7 << endl;
-    cout << str7 + str5 << endl;
+    CheckNum(str1D * str1D, StringNum("2494.0036"), allTestsPass);
+    CheckNum(str2 + str2, StringNum("58.246246"), allTestsPass);
+    CheckNum(str3 * str3, StringNum("999998.000001"), allTestsPass);
+    CheckNum(str2 * str1C, StringNum("58.246246"), allTestsPass);
+    CheckNum(str1C * str2, StringNum("58.246246"), allTestsPass);
+    CheckNum(str7 * str7, StringNum("308880.2929"), allTestsPass);
+    CheckNum(str1C * str1C, StringNum("4.0"), allTestsPass);
+    CheckNum(str4 * str1C, StringNum("19999.9998"), allTestsPass);
 
-    // should be 0.0
-    cout << (str7 + str5) -  (str7 + str5) << endl;
+    CheckNum(str5 * str7, StringNum("-308580.1771"), allTestsPass);
+    CheckNum(str7 * str5, StringNum("-308580.1771"), allTestsPass);
 
-    // should all be -0.54
-    cout << StringNum("0.0") + (str7 + str5) << endl;
-    cout << (str7 + str5) + StringNum() << endl;
-    cout << ((str7 + str5) -  (str7 + str5)) - (str7 + str5) << endl;
-    cout << (str7 + str5) -  ((str7 + str5) - (str7 + str5)) << endl;
+    CheckNum((str7 + str5) -  (str7 + str5), StringNum("0.0"), allTestsPass);
+    CheckNum((str7 + str5) -  (str7 + str5), StringNum("-0.0"), allTestsPass);
 
-    // should be 10999.9989
-    cout << str3 + str4 << endl;
+    CheckNum2((str7 + str5) -  (str7 + str5), "0.0", allTestsPass);
+    CheckNum2((str7 + str5) -  (str7 + str5), "-0", allTestsPass);
+    CheckNum((str7 + str5) -  (str7 + str5), StringNum("-0"), allTestsPass);
+    CheckNum2(StringNum(5), "5", allTestsPass);
+    CheckNum2(StringNum("0.5"), "0.5", allTestsPass);
 
+    CheckNum(StringNum("0.0") + (str7 * str5), StringNum("-308580.1771"), allTestsPass);
+    CheckNum((str7 + str5) + StringNum(), StringNum("-0.54"), allTestsPass);
+    CheckNum(((str7 + str5) -  (str7 + str5)) - (str7 + str5), StringNum("-0.54"), allTestsPass);
+    CheckNum((str7 + str5) -  ((str7 + str5) - (str7 + str5)), StringNum("-0.54"), allTestsPass);
 
-    cout << str2 - str6 << endl; // -526.646877
-    cout << str6 - str2 << endl; // 526.646877
+    CheckNum(str3 + str4, StringNum("10999.9989"), allTestsPass);
 
-    cout << str1-str1B << endl; // -66
+    CheckNum(str2 - str6, StringNum("-526.646877"), allTestsPass);
+    CheckNum(str6 - str2, StringNum("526.646877"), allTestsPass);
 
-
-    cout << "True Side >=" << endl;
-    cout << (str1B >= str1) << " " << (str1B >= str2) << " " << (str4 >= str3) << " " << (str6 >= str5) << endl;
-    cout << (str4 >= str1) << " " << (str4 >= str2) << " " << (str4 >= str3) << " " << (str4 >= str5) << endl;
-    cout << (str4 >= str4) << " " << (str2 >= str2) << " " << (str3 >= str3) << " " << (str5 >= str5) << endl;
-    cout << "False Side >=" << endl;
-    cout << (str1 >= str1B) << " " << (str2 >= str1B) << " " << (str3 >= str4) << " " << (str5 >= str6) << endl;
-    cout << (str1 >= str4) << " " << (str2 >= str4) << " " << (str3 >= str4) << " " << (str5 >= str4) << endl;
-
-    cout << "True Side >" << endl;
-    cout << (str1B > str1) << " " << (str1B > str2) << " " << (str4 > str3) << " " << (str6 > str5) << endl;
-    cout << (str4 > str1) << " " << (str4 > str2) << " " << (str4 > str3) << " " << (str4 > str5) << endl;
-    cout << "False Side >" << endl;
-    cout << (str4 > str4) << " " << (str2 > str2) << " " << (str3 > str3) << " " << (str5 > str5) << endl;
-    cout << (str1 > str1B) << " " << (str2 > str1B) << " " << (str3 > str4) << " " << (str5 > str6) << endl;
-    cout << (str1 > str4) << " " << (str2 > str4) << " " << (str3 > str4) << " " << (str5 > str4) << endl;
+    CheckNumLeftBigger(StringNum(5), "4", allTestsPass);
+    CheckNumLeftBigger(StringNum(5), "-4", allTestsPass);
+    CheckNumLeftBigger(StringNum(1), "0", allTestsPass);
 
 
+    // cout << "True Side >=" << endl;
+    // cout << (str1B >= str1) << " " << (str1B >= str2) << " " << (str4 >= str3) << " " << (str6 >= str5) << endl;
+    // cout << (str4 >= str1) << " " << (str4 >= str2) << " " << (str4 >= str3) << " " << (str4 >= str5) << endl;
+    // cout << (str4 >= str4) << " " << (str2 >= str2) << " " << (str3 >= str3) << " " << (str5 >= str5) << endl;
+    // cout << "False Side >=" << endl;
+    // cout << (str1 >= str1B) << " " << (str2 >= str1B) << " " << (str3 >= str4) << " " << (str5 >= str6) << endl;
+    // cout << (str1 >= str4) << " " << (str2 >= str4) << " " << (str3 >= str4) << " " << (str5 >= str4) << endl;
+    //
+    // cout << "True Side >" << endl;
+    // cout << (str1B > str1) << " " << (str1B > str2) << " " << (str4 > str3) << " " << (str6 > str5) << endl;
+    // cout << (str4 > str1) << " " << (str4 > str2) << " " << (str4 > str3) << " " << (str4 > str5) << endl;
+    // cout << "False Side >" << endl;
+    // cout << (str4 > str4) << " " << (str2 > str2) << " " << (str3 > str3) << " " << (str5 > str5) << endl;
+    // cout << (str1 > str1B) << " " << (str2 > str1B) << " " << (str3 > str4) << " " << (str5 > str6) << endl;
+    // cout << (str1 > str4) << " " << (str2 > str4) << " " << (str3 > str4) << " " << (str5 > str4) << endl;
 
-    str1 = str2 + str1;
-    str1 = str2 - str1;
-    cout << str1 << endl;
-    //str3 = str3 + str4;
-    str3 = str3 - str4;
-    cout << str3 << endl;
-    //str5 = str5 + str6;
-    str5 = str5 - str6;
-    cout << str5 << endl;
+    StringNum str8 = str1;
+    str8 = str2 + str1;
+    str8 = str8 - str2;
+    CheckNum(str8, str1, allTestsPass);
 
+    str8 = str3 + str4;
+    str8 = str8 - str4;
+    CheckNum(str8, str3, allTestsPass);
 
-    StringNum a = 0;
-    StringNum b = 1;
-    StringNum temp = 1;
+    str8 = str5 + str6;
+    str8 = str8 - str6;
+    CheckNum(str8, str5, allTestsPass);
 
+    StringNum a2 = 0;
+    StringNum b2 = 1;
+    StringNum temp2 = 1;
+    int a1 = 0;
+    int b1 = 1;
+    int temp1 = 1;
     for (int i = 1; i <= 9; i++) {
-        temp = a + b;
-        a = b;
-        b = temp;
-        cout << temp << "\n";
+        temp2 = a2 + b2;
+        a2 = b2;
+        b2 = temp2;
+        temp1 = a1 + b1;
+        a1 = b1;
+        b1 = temp1;
+        CheckNum(temp1, temp2, allTestsPass);
     }
 
-    /*
-    StringNum a2 = 100;
-    int x = 100;
+    StringNum a3 = 100;
+    int y = 100;
     for (int i = 1; i <= 150; ++i) {
-        a2 = a2 - i;
-        x = x - i;
-        if (a2 != x) {
-            cout << a2 << " : " << x << endl;
-        }
+        a3 = a3 - i;
+        y = y - i;
+        CheckNum(a3, y, allTestsPass);
     }
 
-    StringNum a2 = 80;
+    StringNum a4 = 80;
     long x = 80;
-    for (int i = 1; i <= 10; ++i) {
-        a2 = a2 * (StringNum(-1) - StringNum(i));
+    for (int i = 1; i <= 6; ++i) {
+        a4 = a4 * (StringNum(-1) - StringNum(i));
         x = x * (-1 - i);
-        if (a2 != x) {
-            cout << a2 << " : " << x << endl;
-        }
+        CheckNum(a4, x, allTestsPass);
     }
-    */
+
+    if (allTestsPass) {
+        cout << "All tests passed" << endl;
+    }
+    else {
+        cout << "At least one test failed" << endl;
+    }
     return 0;
 }
